1ea95e9bacd345681cae571ce7d02aacc1ad8b01
Jabari Dash <jabari.dash@gmail.com>
2017-12-20 22:40:14 -0500

    ok it worked

--- src/test/groovy/structures/vectors/StackSpec.groovy
+++ src/test/groovy/structures/vectors/StackSpec.groovy
@@ -12,7 +12,7 @@ class StackSpec extends Specification {
         Stack stack = new Stack()
 
         expect:
-        stack.size() == 1
+        stack.size() == 0
         stack.empty() == true
     }
 
