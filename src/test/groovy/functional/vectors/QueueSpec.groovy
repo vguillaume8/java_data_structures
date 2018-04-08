482ec6f000d75a88f3cb142467b5ca0a9f82533d
Jabari Dash <jabari.dash@gmail.com>
2018-04-08 02:37:42 -0400

    Finished functional tests for Queue

--- src/test/groovy/functional/vectors/QueueSpec.groovy
+++ src/test/groovy/functional/vectors/QueueSpec.groovy
@@ -7,46 +7,49 @@ import spock.lang.Unroll
 class QueueSpec extends Specification {
 
     /**
-     * https://www.geeksforgeeks.org/implement-stack-using-queue/
-     *
-     * @param <K> Generic type
+     * https://www.geeksforgeeks.org/iterative-method-to-find-height-of-binary-tree/
+     * @return
      */
-    private final class Stack <K> {
-        private Queue<K> inQueue
-        private Queue<K> tempQueue
+    @Unroll
+    def "Height of Binary Search Tree iteratively"() {
+        setup:
+        BinarySearchTree<Integer> tree
 
-        Stack() {
-            inQueue   = new Queue<K>()
-            tempQueue = new Queue<K>()
-        }
+        when:
+        tree = new BinarySearchTree<Integer>(input)
 
-        boolean empty() {
-            return inQueue.empty()
-        }
+        then:
+        tree.size()   == input.size()
+        tree.height() == height
 
-        void push(K element) {
-            inQueue.insert(element)
-        }
+        where:
+        height || input
+        3      || [4, 2, 1, 3, 7, 8]
+        5      || [1, 2, 3, 4, 5]
+        3      || [3, 2, 7, 1, 4, 9]
+    }
 
-        K pop() {
-            K value
+    /**
+     * https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+     * @return
+     */
+    @Unroll
+    def "Breadth First Search on a graph"() {
+        setup:
+        Graph  graph
+        String output
 
-            if (empty()) {
-                throw new Exception("Cannot pop() empty stack")
-            }
+        when:
+        graph = new Graph(input)
+        output = graph.breadthFirstSearch(start)
 
-            while (inQueue.size() != 1) {
-                tempQueue.insert(inQueue.remove())
-            }
+        then:
+        output == BFS
 
-            value = inQueue.remove()
-
-            while (!tempQueue.empty()) {
-                inQueue.insert(tempQueue.remove())
-            }
-
-            return value
-        }
+        where:
+        start || input                                               || BFS
+        2     || [ [0:1],[0:2],[1:2],[2:0],[2:3],[3:3] ]             ||"2 0 3 1"
+        1     || [ [1:2],[1:3],[2:4],[2:5],[3:5],[4:5],[4:6],[5:6] ] || "1 2 3 4 5 6"
     }
 
     /**
@@ -100,4 +103,203 @@ class QueueSpec extends Specification {
         ""                        || ""
         "H"                       || "H"
     }
+
+    private class BinarySearchTree<T extends Comparable> {
+        private Node<T> root
+        private int     size
+
+        int height() {
+            Queue<Node<T>> queue
+            Node<T>        node
+            int            height
+            int            count
+
+            if (root == null) {
+                return 0
+            }
+
+            queue  = new Queue<Node<T>>()
+            queue.insert(root)
+
+            height = 0
+
+            while (true) {
+                count = queue.size()
+
+                if (count == 0) {
+                    return height
+                }
+
+                height++
+
+                while (count > 0) {
+                    node = queue.remove()
+
+                    if (node.left != null) {
+                        queue.insert(node.left)
+                    }
+
+                    if (node.right != null) {
+                        queue.insert(node.right)
+                    }
+
+                    count--
+                }
+            }
+        }
+
+        BinarySearchTree(List<T> values) {
+            for (T value : values) {
+                insert(value)
+            }
+        }
+
+        void insert(T value) {
+            if (root == null) {
+                root = new Node<T>(value)
+            } else {
+                root.insert(value)
+            }
+
+            size++
+        }
+
+        int size() {
+            return this.size
+        }
+
+        private class Node<T extends Comparable> {
+            T       value
+            Node<T> left
+            Node<T> right
+
+            Node(T value) {
+                this.value = value
+                this.left  = null
+                this.right = null
+            }
+
+            void insert(T v) {
+
+                if (v < value) {
+                    if (left == null) {
+                        left = new Node<T>(v)
+
+                    } else {
+                        left.insert(v)
+                    }
+                } else if (v > value) {
+                    if (right == null) {
+                        right = new Node<T>(v)
+                    } else {
+                        right.insert(v)
+                    }
+                }
+            }
+        }
+    }
+
+    private class Graph {
+        private int             numberOfVerticies
+        private List<Integer>[] adjacencyMatrix
+
+        Graph(List<Map<Integer, Integer>> input) {
+
+            this.numberOfVerticies = input.size()
+            this.adjacencyMatrix   = new ArrayList<Integer>[this.numberOfVerticies]
+
+            for (Map<Integer, Integer> map: input) {
+
+                for (Map.Entry<Integer, Integer> entry : map) {
+                    this.addEdge(entry.key, entry.value)
+                }
+            }
+        }
+
+        Graph addEdge(int v, int w) {
+
+            if (adjacencyMatrix[v] == null) {
+                adjacencyMatrix[v] = new ArrayList<Integer>()
+            }
+
+            adjacencyMatrix[v].add(w)
+
+            return this
+        }
+
+        String breadthFirstSearch(int s) {
+            StringBuilder  output
+            boolean[]      visited
+            Queue<Integer> queue
+
+            output  = new StringBuilder()
+            queue   = new Queue<Integer>()
+            visited = new boolean[this.numberOfVerticies]
+
+            for (int i = 0; i < visited.length; i++) {
+                visited[i] = false
+            }
+
+            visited[s] = true
+            queue.insert(s)
+
+            while (!queue.empty()) {
+                s = queue.remove()
+                output.append(s + " ")
+
+                for (Integer i : adjacencyMatrix[s]) {
+                    if (!visited[i]) {
+                        visited[i] = true
+                        queue.insert(i)
+                    }
+                }
+            }
+
+            return output.toString().substring(0, output.length()-1)
+        }
+    }
+
+    /**
+     * https://www.geeksforgeeks.org/implement-stack-using-queue/
+     *
+     * @param <K> Generic type
+     */
+    private final class Stack <K> {
+        private Queue<K> inQueue
+        private Queue<K> tempQueue
+
+        Stack() {
+            inQueue   = new Queue<K>()
+            tempQueue = new Queue<K>()
+        }
+
+        boolean empty() {
+            return inQueue.empty()
+        }
+
+        void push(K element) {
+            inQueue.insert(element)
+        }
+
+        K pop() {
+            K value
+
+            if (empty()) {
+                throw new Exception("Cannot pop() empty stack")
+            }
+
+            while (inQueue.size() != 1) {
+                tempQueue.insert(inQueue.remove())
+            }
+
+            value = inQueue.remove()
+
+            while (!tempQueue.empty()) {
+                inQueue.insert(tempQueue.remove())
+            }
+
+            return value
+        }
+    }
+
 }
