01686e34323e7bd9c57c6dcf53db84f5a0357cd7
Jabari Dash <jabari.dash@gmail.com>
2018-04-16 02:58:01 -0400

    renamed .travis.sh to travis.sh

--- src/test/groovy/structures/unit/commons/DynamicArray.groovy
+++ src/test/groovy/structures/unit/commons/DynamicArray.groovy
@@ -1,7 +1,31 @@
 package structures.unit.commons
 
+import structures.vectors.ArrayList
+import structures.vectors.ArrayQueue
+import structures.vectors.ArrayStack
 import util.Spec
 
-abstract class DynamicArray extends Spec {
+abstract class DynamicArraySpec extends Spec {
 
+}
+
+final class DynamicArraySpec_ArrayList extends DynamicArraySpec {
+
+    def setup() {
+        myClass = ArrayList
+    }
+}
+
+final class DynamicArraySpec_ArrayStack extends DynamicArraySpec {
+
+    def setup() {
+        myClass = ArrayStack
+    }
+}
+
+final class DynamicArraySpec_ArrayQueue extends DynamicArraySpec {
+
+    def setup() {
+        myClass = ArrayQueue
+    }
 }
